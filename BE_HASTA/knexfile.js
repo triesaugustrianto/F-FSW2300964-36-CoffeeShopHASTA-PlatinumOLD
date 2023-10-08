892b0211d79c27411e5a3e8984b39fc4bbacba6a
unknown <hidayatullah1780@gmail.com>
2023-10-08 15:07:13 +0700

    revisi json

--- BE_HASTA/knexfile.js
+++ BE_HASTA/knexfile.js
@@ -6,30 +6,12 @@
 module.exports = {
   //development
 
-  // development: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "db_HastaCoffeeShop",
-  //     user: "admin",
-  //     password: "admin",
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations",
-  //   },
-  // },
-
-  //===================================================
-  //production
   development: {
     client: "postgresql",
     connection: {
-      database: "db_coffe",
-      user: "postgres",
-      password: "123456",
+      database: "db_HastaCoffeeShop",
+      user: "admin",
+      password: "admin",
     },
     pool: {
       min: 2,
@@ -39,4 +21,22 @@ module.exports = {
       tableName: "knex_migrations",
     },
   },
+
+  //===================================================
+  //production
+  // development: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "db_coffe",
+  //     user: "postgres",
+  //     password: "123456",
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations",
+  //   },
+  // },
 };
