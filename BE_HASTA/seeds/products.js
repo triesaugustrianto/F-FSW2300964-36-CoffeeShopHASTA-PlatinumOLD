a38ff1fe3ba2dd0083beadc1458ee41fef5bd4a1
Tries Augustrianto <triesatrianto@gmail.com>
2023-10-09 11:31:03 +0700

    fixed create products error duplicate

--- BE_HASTA/seeds/products.js
+++ BE_HASTA/seeds/products.js
@@ -7,7 +7,6 @@ exports.seed = async function (knex) {
   await knex("products").del();
   await knex("products").insert([
     {
-      id: 1,
       name: "gula aren",
       price: 18000,
       stock: 100,
@@ -16,7 +15,6 @@ exports.seed = async function (knex) {
       description: "kopi gula aren dengan susu yang fresh",
     },
     {
-      id: 2,
       name: "latte",
       price: 18000,
       stock: 100,
@@ -25,7 +23,6 @@ exports.seed = async function (knex) {
       description: "kopi latte dengan susu yang fresh",
     },
     {
-      id: 3,
       name: "mocca",
       price: 18000,
       stock: 100,
@@ -34,7 +31,6 @@ exports.seed = async function (knex) {
       description: "kopi mocca dengan susu yang fresh",
     },
     {
-      id: 4,
       name: "juice alamaho",
       price: 18000,
       stock: 100,
@@ -43,7 +39,6 @@ exports.seed = async function (knex) {
       description: "buah segar dengan susu yang fresh",
     },
     {
-      id: 5,
       name: "juice caciagoe",
       price: 18000,
       stock: 100,
@@ -52,7 +47,6 @@ exports.seed = async function (knex) {
       description: "buah segar dengan susu yang fresh",
     },
     {
-      id: 6,
       name: "tea dilmah",
       price: 18000,
       stock: 100,
@@ -61,7 +55,6 @@ exports.seed = async function (knex) {
       description: "daun teh alami",
     },
     {
-      id: 7,
       name: "tea tongjie",
       price: 18000,
       stock: 100,
