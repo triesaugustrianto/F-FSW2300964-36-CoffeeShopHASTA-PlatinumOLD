43a642b20f8014ce559cb009fcd631c5b755893d
hidayatullah <hidayatullah1780@gmail.com>
2023-10-20 08:42:22 +0700

    create checkout

---
+++ BE_HASTA/migrations/20231020005354_items.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("items", (table) => {
+    table.increments("id");
+    table.string("name");
+    table.string("qty");
+    table.string("keterangan");
+    table.timestamp("createdAt").defaultTo(knex.fn.now());
+    table.integer("id_transaksi").unsigned().notNullable();
+    table
+      .foreign("id_transaksi")
+      .references("id")
+      .inTable("transaction")
+      .onDelete("CASCADE");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("items");
+};
